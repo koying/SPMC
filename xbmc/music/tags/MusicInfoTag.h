58790fb52939dae6c63fc6fee8548d555b3a0b3a
Chris "Koying" Browet <cbro@semperpax.com>
2012-10-09 12:14:53 +0200

    CHG: refine destination filename when copying from DB

--- xbmc/music/tags/MusicInfoTag.h
+++ xbmc/music/tags/MusicInfoTag.h
@@ -175,7 +175,6 @@ public:
 
   void Clear();
 
-protected:
   /*! \brief Trim whitespace off the given string
    \param value string to trim
    \return trimmed value, with spaces removed from left and right, as well as carriage returns from the right.
