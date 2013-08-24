2b16e4257d42bcbe7ca25cafb68022b4d5605e25
Chris "Koying" Browet <cbro@semperpax.com>
2013-08-24 10:30:54 +0200

    ADD; [droid] Partial joystick support (buttons only; generic keymap only)

--- xbmc/android/activity/AndroidExtra.h
+++ xbmc/android/activity/AndroidExtra.h
@@ -24,3 +24,15 @@
  //Additional defines from android.view.KeyEvent (http://developer.android.com/reference/android/view/KeyEvent.html)
 #define AKEYCODE_ESCAPE 111
 #define AKEYCODE_FORWARD_DEL 112
+ 
+//Additional defines from android.view.InputDevice (http://developer.android.com/reference/android/view/InputDevice.html)
+#define AINPUT_SOURCE_GAMEPAD 0x00000400
+#define AINPUT_SOURCE_JOYSTICK 0x01000010
+
+// Joystick Axes (http://developer.android.com/reference/android/view/MotionEvent.html)
+#define AMOTION_EVENT_AXIS_X 0
+#define AMOTION_EVENT_AXIS_Y 1
+#define AMOTION_EVENT_AXIS_LTRIGGER 17
+#define AMOTION_EVENT_AXIS_RTRIGGER 18
+#define AMOTION_EVENT_AXIS_Z 11  //2nd stick X
+#define AMOTION_EVENT_AXIS_RZ 14  //2nd stick Y
