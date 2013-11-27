a37a632e5521ed3cddc9f6f305bc35988dcbe828
Chris "Koying" Browet <cbro@semperpax.com>
2013-11-27 17:52:28 +0100

    Revert "contd mutichannel"
    This reverts commit 704231531a3ed6cd23f820da59709add5a15710c.

--- tools/android/packaging/xbmc/src/org/xbmc/xbmc/Main.java
+++ tools/android/packaging/xbmc/src/org/xbmc/xbmc/Main.java
@@ -3,55 +3,19 @@ package com.semperpax.spmc;
 import android.app.NativeActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.media.AudioManager;
-import android.content.Context;
-import android.util.Log;
 
-public class Main extends NativeActivity implements AudioManager.OnAudioFocusChangeListener
+public class Main extends NativeActivity 
 {
   native void _onNewIntent(Intent intent);
-  
-  private static final String TAG = "Main";
-
   public Main() 
   {
     super();
   }
 
   @Override
-  public void onAudioFocusChange(int focusChange) 
-  {
-    if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT)
-    {
-      Log.i(TAG, "Lost audio focus: transient");
-    } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) 
-    {
-      Log.i(TAG, "Got back audio focus");
-    } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS)
-    {
-      Log.w(TAG, "Lost audio focus");
-      AudioManager am = (AudioManager)this.getSystemService(Context.AUDIO_SERVICE);
-      am.abandonAudioFocus(this);
-    }
-  }
-
-  @Override
   public void onCreate(Bundle savedInstanceState) 
   {
     super.onCreate(savedInstanceState);
-
-    AudioManager am = (AudioManager)this.getSystemService(Context.AUDIO_SERVICE);
-
-    int result = am.requestAudioFocus(this,
-                                     // Use the music stream.
-                                     AudioManager.STREAM_MUSIC,
-                                     // Request permanent focus.
-                                     AudioManager.AUDIOFOCUS_GAIN);
-
-    if (result != AudioManager.AUDIOFOCUS_REQUEST_GRANTED)
-    {
-      Log.w(TAG, "Cannot get audio focus");
-    }
   }
 
   @Override
@@ -71,10 +35,6 @@ public class Main extends NativeActivity implements AudioManager.OnAudioFocusCha
         } catch (Exception e) {}
       }
     }.start();
-    
-    AudioManager am = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
-    am.abandonAudioFocus(this);
-    
     super.onDestroy();
   }
 }
