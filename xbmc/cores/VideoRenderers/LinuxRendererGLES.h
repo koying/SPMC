77f4835430931d4dfb9fb2740876c101b5f74f4d
Chris "Koying" Browet <cbro@semperpax.com>
2013-12-14 20:26:47 +0100

    ADD: [stagefright] refactor + add rockchip specific implem

--- xbmc/cores/VideoRenderers/LinuxRendererGLES.h
+++ xbmc/cores/VideoRenderers/LinuxRendererGLES.h
@@ -40,7 +40,7 @@ class CBaseTexture;
 namespace Shaders { class BaseYUV2RGBShader; }
 namespace Shaders { class BaseVideoFilterShader; }
 class COpenMaxVideo;
-class CDVDVideoCodecStageFright;
+class CDVDVideoCodecStageFrightBuffer;
 class CDVDMediaCodecInfo;
 typedef std::vector<int>     Features;
 
@@ -163,7 +163,7 @@ public:
   virtual void         AddProcessor(struct __CVBuffer *cvBufferRef, int index);
 #endif
 #ifdef HAS_LIBSTAGEFRIGHT
-  virtual void         AddProcessor(CDVDVideoCodecStageFright* stf, EGLImageKHR eglimg, int index);
+  virtual void         AddProcessor(CDVDVideoCodecStageFrightBuffer* stfbuf, int index);
 #endif
 #if defined(TARGET_ANDROID)
   // mediaCodec
@@ -194,6 +194,8 @@ protected:
   void UploadNV12Texture(int index);
   void DeleteNV12Texture(int index);
   bool CreateNV12Texture(int index);
+
+  void UploadStfBufTexture(int index);
 
   void UploadCVRefTexture(int index);
   void DeleteCVRefTexture(int index);
@@ -285,8 +287,7 @@ protected:
     struct __CVBuffer *cvBufferRef;
 #endif
 #ifdef HAS_LIBSTAGEFRIGHT
-    CDVDVideoCodecStageFright* stf;
-    EGLImageKHR eglimg;
+    CDVDVideoCodecStageFrightBuffer* stfbuf;
 #endif
 #if defined(TARGET_ANDROID)
     // mediacodec
---
+++ xbmc/cores/dvdplayer/DVDCodecs/DVDCodecInterface.cpp
@@ -0,0 +1,50 @@
+/*
+ *      Copyright (C) 2014 Team Kodi
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "DVDCodecInterface.h"
+
+#include "Application.h"
+#include "ApplicationMessenger.h"
+#include "windowing/WindowingFactory.h"
+#include "settings/AdvancedSettings.h"
+
+CDVDCodecInterface::CDVDCodecInterface()
+{
+}
+
+CApplication* CDVDCodecInterface::GetApplication() const
+{
+  return &g_application;
+}
+
+CApplicationMessenger* CDVDCodecInterface::GetApplicationMessenger() const
+{
+  return &CApplicationMessenger::Get();
+}
+
+CWinSystemBase* CDVDCodecInterface::GetWindowSystem() const
+{
+  return &g_Windowing;
+}
+
+CAdvancedSettings* CDVDCodecInterface::GetAdvancedSettings() const
+{
+  return &g_advancedSettings;
+}
