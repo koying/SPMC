7dc86fb7ad6c57a06d9f221d017becabe87fa20a
Chris "Koying" Browet <cbro@semperpax.com>
2014-02-06 16:27:08 +0100

    FIX: do not popup OSD on DVD menu with touch or mouse

--- xbmc/cores/dvdplayer/DVDPlayer.cpp
+++ xbmc/cores/dvdplayer/DVDPlayer.cpp
@@ -3729,7 +3729,15 @@ bool CDVDPlayer::OnAction(const CAction &action)
           pt.y *= rs.Height() / rd.Height();
           pt += CPoint(rs.x1, rs.y1);
           if (action.GetID() == ACTION_MOUSE_LEFT_CLICK)
-            return pMenus->OnMouseClick(pt);
+          {
+            if (pMenus->OnMouseClick(pt))
+              return true;
+            else
+            {
+              CApplicationMessenger::Get().SendAction(CAction(ACTION_TRIGGER_OSD), WINDOW_INVALID, false); // Trigger the osd
+              return false;
+            }
+          }
           return pMenus->OnMouseMove(pt);
         }
         break;
@@ -3825,6 +3833,10 @@ bool CDVDPlayer::OnAction(const CAction &action)
       }
       else
         break;
+    case ACTION_MOUSE_MOVE:
+    case ACTION_MOUSE_LEFT_CLICK:
+      CApplicationMessenger::Get().SendAction(CAction(ACTION_TRIGGER_OSD), WINDOW_INVALID, false);
+      return false;
   }
 
   // return false to inform the caller we didn't handle the message
