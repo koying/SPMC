72b3d0412dcc6cffa1b74b80dffc0878e3d32b4c
Chris "Koying" Browet <cbro@semperpax.com>
2014-10-21 16:34:47 +0200

    ADD: [droid;aml] specific aml + android windowing

---
+++ xbmc/windowing/egl/EGLNativeTypeAmlAndroid.cpp
@@ -0,0 +1,132 @@
+/*
+ *      Copyright (C) 2011-2014 Team XBMC
+ *      http://xbmc.org
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XBMC; see the file COPYING.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "system.h"
+#include <EGL/egl.h>
+#include "EGLNativeTypeAmlAndroid.h"
+#include "utils/log.h"
+#include "guilib/gui3d.h"
+#if defined(TARGET_ANDROID)
+  #include "android/activity/XBMCApp.h"
+#endif
+#include "utils/StringUtils.h"
+#include "utils/AMLUtils.h"
+
+
+bool CEGLNativeTypeAmlAndroid::CheckCompatibility()
+{
+#if defined(TARGET_ANDROID)
+  return aml_present();
+#endif
+  return false;
+}
+
+bool CEGLNativeTypeAmlAndroid::GetNativeResolution(RESOLUTION_INFO *res) const
+{
+  char mode[256] = {0};
+  aml_get_sysfs_str("/sys/class/display/mode", mode, 255);
+  return aml_ModeToResolution(mode, res);
+}
+
+bool CEGLNativeTypeAmlAndroid::SetNativeResolution(const RESOLUTION_INFO &res)
+{
+  switch((int)(0.5 + res.fRefreshRate))
+  {
+    default:
+    case 60:
+      switch(res.iScreenWidth)
+      {
+        default:
+        case 1280:
+          SetDisplayResolution("720p");
+          break;
+        case 1920:
+          if (res.dwFlags & D3DPRESENTFLAG_INTERLACED)
+            SetDisplayResolution("1080i");
+          else
+            SetDisplayResolution("1080p");
+          break;
+      }
+      break;
+    case 50:
+      switch(res.iScreenWidth)
+      {
+        default:
+        case 1280:
+          SetDisplayResolution("720p50hz");
+          break;
+        case 1920:
+          if (res.dwFlags & D3DPRESENTFLAG_INTERLACED)
+            SetDisplayResolution("1080i50hz");
+          else
+            SetDisplayResolution("1080p50hz");
+          break;
+      }
+      break;
+    case 30:
+      SetDisplayResolution("1080p30hz");
+      break;
+    case 24:
+      SetDisplayResolution("1080p24hz");
+      break;
+  }
+
+  return true;
+}
+
+bool CEGLNativeTypeAmlAndroid::ProbeResolutions(std::vector<RESOLUTION_INFO> &resolutions)
+{
+  char valstr[256] = {0};
+  aml_get_sysfs_str("/sys/class/amhdmitx/amhdmitx0/disp_cap", valstr, 255);
+  std::vector<CStdString> probe_str;
+  StringUtils::SplitString(valstr, "\n", probe_str);
+
+  resolutions.clear();
+  RESOLUTION_INFO res;
+  for (size_t i = 0; i < probe_str.size(); i++)
+  {
+    if(aml_ModeToResolution(probe_str[i].c_str(), &res))
+      resolutions.push_back(res);
+  }
+  return resolutions.size() > 0;
+
+}
+
+bool CEGLNativeTypeAmlAndroid::GetPreferredResolution(RESOLUTION_INFO *res) const
+{
+  // check display/mode, it gets defaulted at boot
+  if (!GetNativeResolution(res))
+  {
+    // punt to 720p if we get nothing
+    aml_ModeToResolution("720p", res);
+  }
+
+  return true;
+}
+
+bool CEGLNativeTypeAmlAndroid::SetDisplayResolution(const char *resolution)
+{
+  CStdString mode = resolution;
+  // switch display resolution
+  aml_set_sysfs_str("/sys/class/display/mode", mode.c_str());
+
+  return true;
+}
+
