2411199d106dfcd4d667321df9416847d7cdae1c
Chris "Koying" Browet <cbro@semperpax.com>
2014-10-25 14:14:32 +0200

    [minix] force setting level to expert

--- xbmc/view/ViewStateSettings.cpp
+++ xbmc/view/ViewStateSettings.cpp
@@ -117,11 +117,14 @@ bool CViewStateSettings::Load(const TiXmlNode *settings)
   pElement = settings->FirstChild(XML_GENERAL);
   if (pElement != NULL)
   {
+    /*
+    // Force setting level to expert
     int settingLevel;
     if (XMLUtils::GetInt(pElement, XML_SETTINGLEVEL, settingLevel, (const int)SettingLevelBasic, (const int)SettingLevelExpert))
       m_settingLevel = (SettingLevel)settingLevel;
     else
-      m_settingLevel = SettingLevelStandard;
+    */
+      m_settingLevel = SettingLevelExpert;
   }
 
   return true;
