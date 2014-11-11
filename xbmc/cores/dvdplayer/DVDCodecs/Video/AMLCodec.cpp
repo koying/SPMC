9bc80b38c4bbc4677bd80fbb5b3a3e77aede986c
Chris "Koying" Browet <cbro@semperpax.com>
2014-11-11 14:46:54 +0100

    CHG: Add Lock, Release and context to Render callbacks

--- xbmc/cores/dvdplayer/DVDCodecs/Video/AMLCodec.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/AMLCodec.cpp
@@ -2392,7 +2392,7 @@ void CAMLCodec::SetVideoRect(const CRect &SrcRect, const CRect &DestRect)
   ShowMainVideo(true);
 }
 
-void CAMLCodec::RenderUpdateCallBack(const void *ctx, const CRect &SrcRect, const CRect &DestRect)
+void CAMLCodec::RenderUpdateCallBack(const void *ctx, const CRect &SrcRect, const CRect &DestRect, const void*)
 {
   CAMLCodec *codec = (CAMLCodec*)ctx;
   codec->SetVideoRect(SrcRect, DestRect);
