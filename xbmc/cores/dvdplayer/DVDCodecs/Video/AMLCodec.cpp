f87440c97cd990c7c72abfad283798549a7a1a6e
Chris "Koying" Browet <cbro@semperpax.com>
2014-11-28 11:05:28 +0100

    FIX: [aml] cleanup freescale

--- xbmc/cores/dvdplayer/DVDCodecs/Video/AMLCodec.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/AMLCodec.cpp
@@ -1443,25 +1443,6 @@ int set_header_info(am_private_t *para)
   return PLAYER_SUCCESS;
 }
 
-void UnscaleMx6Video(bool on)
-{
-  aml_set_sysfs_int("/sys/class/graphics/fb0/blank", 1);
-  aml_set_sysfs_int("/sys/class/graphics/fb0/free_scale", 0);
-  aml_set_sysfs_int("/sys/class/graphics/fb1/free_scale", 0);
-  aml_set_sysfs_int("/sys/class/ppmgr/ppscaler", 0);
-
-  if (on)
-  {
-    aml_set_sysfs_str("/sys/class/graphics/fb0/request2XScale", "8");
-    aml_set_sysfs_str("/sys/class/graphics/fb1/scale_axis", "1280 720 1920 1080");
-    aml_set_sysfs_str("/sys/class/graphics/fb1/scale", "0x10001");
-  }
-  else
-    aml_set_sysfs_int("/sys/class/graphics/fb0/blank", 0);
-
-  aml_set_sysfs_int("/sys/class/graphics/fb0/blank", 0);
-}
-
 /*************************************************************************/
 CAMLCodec::CAMLCodec() : CThread("CAMLCodec")
 {
@@ -1501,7 +1482,6 @@ bool CAMLCodec::OpenDecoder(CDVDStreamInfo &hints)
   m_start_dts = 0;
   m_start_pts = 0;
   m_hints = hints;
-  m_freescaled = false;
 
   ShowMainVideo(false);
 
@@ -1712,29 +1692,12 @@ bool CAMLCodec::OpenDecoder(CDVDStreamInfo &hints)
   g_renderManager.RegisterRenderUpdateCallBack((const void*)this, RenderUpdateCallBack);
   g_renderManager.RegisterRenderFeaturesCallBack((const void*)this, RenderFeaturesCallBack);
 
-  m_display_rect = CRect();
+  m_display_rect = g_graphicsContext.GetViewWindow();
   char mode[256] = {0};
   aml_get_sysfs_str("/sys/class/display/mode", mode, 255);
   RESOLUTION_INFO res;
   if (aml_ModeToResolution(mode, &res))
-  {
     m_display_rect = CRect(0, 0, res.iScreenWidth, res.iScreenHeight);
-/*
-    if (aml_get_device_type() >= AML_DEVICE_TYPE_M8)
-    {
-      m_display_rect = CRect(0, 0, res.iScreenWidth, res.iScreenHeight);
-    }
-    else
-    {
-      if (res.iHeight > 720 && hints.height > 720)
-      {
-        m_freescaled = true;
-        UnscaleMx6Video(true);
-        m_display_rect = CRect(0, 0, res.iScreenWidth, res.iScreenHeight);
-      }
-    }
-*/
-  }
 
 /*
   // if display is set to 1080xxx, then disable deinterlacer for HD content
@@ -1780,8 +1743,6 @@ void CAMLCodec::CloseDecoder()
   aml_set_sysfs_int("/sys/class/tsync/enable", 1);
 
   SetVideoHdmi3dMode(MODE_HDMI3D_OFF);
-  if (m_freescaled)
-    UnscaleMx6Video(false);
   ShowMainVideo(false);
 }
 
