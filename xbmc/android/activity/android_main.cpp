df8ce521cefd34e04c2254868c997ee35e38690a
Chris "Koying" Browet <cbro@semperpax.com>
2014-12-27 17:52:05 +0100

    [SPMC] branding

--- xbmc/android/activity/android_main.cpp
+++ xbmc/android/activity/android_main.cpp
@@ -86,9 +86,9 @@ extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved)
 
   std::string appName = CCompileInfo::GetAppName();
   StringUtils::ToLower(appName);
-  std::string mainClass = "org/xbmc/" + appName + "/Main";
-  std::string bcReceiver = "org/xbmc/" + appName + "/XBMCBroadcastReceiver";
-  std::string frameListener = "org/xbmc/" + appName + "/XBMCOnFrameAvailableListener";
+  std::string mainClass = "com/semperpax/" + appName + "/Main";
+  std::string bcReceiver = "com/semperpax/" + appName + "/XBMCBroadcastReceiver";
+  std::string frameListener = "com/semperpax/" + appName + "/XBMCOnFrameAvailableListener";
 
   jclass cMain = env->FindClass(mainClass.c_str());
   if(cMain)
