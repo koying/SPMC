76208f7c60e671bd7ac0cf828d28f3aa465548a2
Chris "Koying" Browet <cbro@semperpax.com>
2014-12-28 11:55:18 +0100

    fixup sd mp4

--- xbmc/cores/dvdplayer/DVDCodecs/DVDFactoryCodec.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/DVDFactoryCodec.cpp
@@ -211,9 +211,9 @@ CDVDVideoCodec* CDVDFactoryCodec::CreateVideoCodec(CDVDStreamInfo &hint, unsigne
      if ( (pCodec = OpenCodec(new CDVDVideoCodecLibMpeg2(), hint, options)) ) return pCodec;
   }
 
+#if defined(HAS_LIBAMCODEC)
   if ((EDECODEMETHOD) CSettings::Get().GetInt("videoplayer.decodingmethod") == VS_DECODEMETHOD_HARDWARE)
   {
-#if defined(HAS_LIBAMCODEC)
     // amcodec can handle dvd playback.
     if (!hint.software && CSettings::Get().GetBool("videoplayer.useamcodec"))
     {
