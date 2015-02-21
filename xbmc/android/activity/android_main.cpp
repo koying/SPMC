58bef2286eeed4518dd227b6b668762a11b0fa3a
Chris "Koying" Browet <cbro@semperpax.com>
2015-02-21 13:33:34 +0100

    CHG: [droid] let the system handle volume

--- xbmc/android/activity/android_main.cpp
+++ xbmc/android/activity/android_main.cpp
@@ -89,6 +89,7 @@ extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved)
   std::string mainClass = "com/semperpax/" + appName + "/Main";
   std::string bcReceiver = "com/semperpax/" + appName + "/XBMCBroadcastReceiver";
   std::string frameListener = "com/semperpax/" + appName + "/XBMCOnFrameAvailableListener";
+  std::string settingsObserver = "com/semperpax/" + appName + "/XBMCSettingsContentObserver";
 
   jclass cMain = env->FindClass(mainClass.c_str());
   if(cMain)
@@ -121,6 +122,17 @@ extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved)
       (void*)&CJNISurfaceTextureOnFrameAvailableListener::_onFrameAvailable
     };
     env->RegisterNatives(cFrameAvailableListener, &mOnFrameAvailable, 1);
+  }
+
+  jclass cSettingsObserver = env->FindClass(settingsObserver.c_str());
+  if(cSettingsObserver)
+  {
+    JNINativeMethod mOnVolumeChanged = {
+      "_onVolumeChanged",
+      "(I)V",
+      (void*)&CJNIApplicationMainActivity::_onVolumeChanged
+    };
+    env->RegisterNatives(cSettingsObserver, &mOnVolumeChanged, 1);
   }
 
   return version;
