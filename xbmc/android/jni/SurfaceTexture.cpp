25f30256043321a3858f0556600b335cf5b6f27f
Chris "Koying" Browet <cbro@semperpax.com>
2015-04-03 08:14:18 +0200

    [spmc] branding v15

--- xbmc/android/jni/SurfaceTexture.cpp
+++ xbmc/android/jni/SurfaceTexture.cpp
@@ -35,7 +35,7 @@ using namespace jni;
 CJNISurfaceTextureOnFrameAvailableListener* CJNISurfaceTextureOnFrameAvailableListener::m_listenerInstance(NULL);
 
 CJNISurfaceTextureOnFrameAvailableListener::CJNISurfaceTextureOnFrameAvailableListener()
-: CJNIBase("com/semperpax/spmc/XBMCOnFrameAvailableListener")
+: CJNIBase("com/semperpax/spmc15/XBMCOnFrameAvailableListener")
 {
   CJNIApplicationMainActivity *appInstance = CJNIApplicationMainActivity::GetAppInstance();
   if (!appInstance)
