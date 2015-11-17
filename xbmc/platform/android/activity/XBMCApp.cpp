aea99942c4cbb6c0bbfd92cd6b9afb3a3ccc7955
Chris Browet <koying@semperpax.com>
2015-11-17 13:01:08 +0100

    CHG: [droid] cleanup XBMCApp

--- xbmc/platform/android/activity/XBMCApp.cpp
+++ xbmc/platform/android/activity/XBMCApp.cpp
@@ -233,7 +233,7 @@ void CXBMCApp::onDestroy()
   // been destroyed.
   if (!m_exiting)
   {
-    XBMC_Stop();
+    CApplicationMessenger::GetInstance().PostMsg(TMSG_QUIT);
     pthread_join(m_thread, NULL);
     android_printf(" => XBMC finished");
   }
@@ -270,7 +270,6 @@ void CXBMCApp::onCreateWindow(ANativeWindow* window)
   if(!m_firstrun)
   {
     XBMC_SetupDisplay();
-    XBMC_Pause(false);
   }
 }
 
@@ -291,7 +290,6 @@ void CXBMCApp::onDestroyWindow()
   {
     XBMC_DestroyDisplay();
     m_window = NULL;
-    XBMC_Pause(true);
   }
 }
 
@@ -444,16 +442,6 @@ void CXBMCApp::run()
   // onPause(), onLostFocus(), onDestroyWindow(), onStop(), onDestroy().
   ANativeActivity_finish(m_activity);
   m_exiting=true;
-}
-
-void CXBMCApp::XBMC_Pause(bool pause)
-{
-  android_printf("XBMC_Pause(%s)", pause ? "true" : "false");
-}
-
-void CXBMCApp::XBMC_Stop()
-{
-  CApplicationMessenger::GetInstance().PostMsg(TMSG_QUIT);
 }
 
 bool CXBMCApp::XBMC_SetupDisplay()
