536b21e3a8d345c7d451597bbbadfd18b7e52fef
Chris "Koying" Browet <cbro@semperpax.com>
2015-12-01 22:54:28 +0100

    FIX: [droid] limit float to stereo

--- xbmc/cores/AudioEngine/Sinks/AESinkAUDIOTRACK.cpp
+++ xbmc/cores/AudioEngine/Sinks/AESinkAUDIOTRACK.cpp
@@ -256,8 +256,8 @@ bool CAESinkAUDIOTRACK::Initialize(AEAudioFormat &format, std::string &device)
   else
   {
     m_passthrough = false;
-    if (CJNIAudioManager::GetSDKVersion() >= 21)
-     {
+    if (CJNIAudioManager::GetSDKVersion() >= 21 && m_format.m_channelLayout.Count() == 2)
+    {
       m_encoding = CJNIAudioFormat::ENCODING_PCM_FLOAT;
       m_format.m_dataFormat     = AE_FMT_FLOAT;
     }
