3a6dcb06f98e6df912eec115000ebed945656ed7
Chris "Koying" Browet <cbro@semperpax.com>
2016-01-31 15:51:53 +0100

    ADD: [droid] package root in version.txt

--- xbmc/platform/android/activity/XBMCApp.cpp
+++ xbmc/platform/android/activity/XBMCApp.cpp
@@ -317,7 +317,9 @@ bool CXBMCApp::EnableWakeLock(bool on)
   {
     std::string appName = CCompileInfo::GetAppName();
     StringUtils::ToLower(appName);
-    std::string className = "org.xbmc." + appName;
+    std::string pkgRoot = CCompileInfo::GetPkgRoot();
+    StringUtils::ToLower(pkgRoot);
+    std::string className = pkgRoot + "." + appName;
     // SCREEN_BRIGHT_WAKE_LOCK is marked as deprecated but there is no real alternatives for now
     m_wakeLock = new CJNIWakeLock(CJNIPowerManager(getSystemService("power")).newWakeLock(CJNIPowerManager::SCREEN_BRIGHT_WAKE_LOCK, className.c_str()));
     if (m_wakeLock)
--- xbmc/platform/android/activity/android_main.cpp
+++ xbmc/platform/android/activity/android_main.cpp
@@ -134,12 +134,15 @@ extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved)
 
   std::string appName = CCompileInfo::GetAppName();
   StringUtils::ToLower(appName);
-  std::string mainClass = "org/xbmc/" + appName + "/Main";
-  std::string bcReceiver = "org/xbmc/" + appName + "/XBMCBroadcastReceiver";
-  std::string frameListener = "org/xbmc/" + appName + "/XBMCOnFrameAvailableListener";
-  std::string settingsObserver = "org/xbmc/" + appName + "/XBMCSettingsContentObserver";
-  std::string audioFocusChangeListener = "org/xbmc/" + appName + "/XBMCOnAudioFocusChangeListener";
-  std::string inputDeviceListener = "org/xbmc/" + appName + "/XBMCInputDeviceListener";
+  std::string pkgRoot = CCompileInfo::GetPkgRoot();
+  StringUtils::ToLower(pkgRoot);
+  StringUtils::Replace(pkgRoot, '.', '/');
+  std::string mainClass = pkgRoot + "/" + appName + "/Main";
+  std::string bcReceiver = pkgRoot + "/" + appName + "/XBMCBroadcastReceiver";
+  std::string frameListener = pkgRoot + "/" + appName + "/XBMCOnFrameAvailableListener";
+  std::string settingsObserver = pkgRoot + "/" + appName + "/XBMCSettingsContentObserver";
+  std::string audioFocusChangeListener = pkgRoot + "/" + appName + "/XBMCOnAudioFocusChangeListener";
+  std::string inputDeviceListener = pkgroot + "/" + appName + "/XBMCInputDeviceListener";
 
   jclass cMain = env->FindClass(mainClass.c_str());
   if(cMain)
