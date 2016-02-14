f8aab58e34c01f9c5cd29de291b4e76981e736ba
Chris "Koying" Browet <cbro@semperpax.com>
2016-02-14 17:09:03 +0100

    CHG: [droid] only stop on lost focus with mediacodec surface

--- xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecAndroidMediaCodec.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecAndroidMediaCodec.cpp
@@ -489,6 +489,7 @@ bool CDVDVideoCodecAndroidMediaCodec::Open(CDVDStreamInfo &hints, CDVDCodecOptio
     m_videosurface = CXBMCApp::get()->getVideoViewSurface();
     if (!m_videosurface)
       return false;
+    CXBMCApp::get()->setVideosurfaceInUse(true);
   }
 
   if (m_render_surface)
@@ -659,6 +660,7 @@ void CDVDVideoCodecAndroidMediaCodec::Dispose()
   ReleaseSurfaceTexture();
   if (m_render_surface)
     CXBMCApp::get()->clearVideoView();
+  CXBMCApp::get()->setVideosurfaceInUse(false);
 
   SAFE_DELETE(m_bitstream);
 }
