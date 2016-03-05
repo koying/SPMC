a4351986f419bc1390059e5f8df8ba08ff25edae
Chris "Koying" Browet <cbro@semperpax.com>
2016-03-05 21:35:39 +0100

    CHG: [droid] include full package name in version.txt (+ un-static java)

--- xbmc/filesystem/AndroidAppDirectory.cpp
+++ xbmc/filesystem/AndroidAppDirectory.cpp
@@ -49,9 +49,8 @@ bool CAndroidAppDirectory::GetDirectory(const CURL& url, CFileItemList &items)
   CLog::Log(LOGDEBUG, "CAndroidAppDirectory::GetDirectory: %s",dirname.c_str()); 
   std::string appName = CCompileInfo::GetAppName();
   StringUtils::ToLower(appName);
-  std::string pkgRoot = CCompileInfo::GetPkgRoot();
-  StringUtils::ToLower(pkgRoot);
-  std::string className = pkgRoot + "." + appName;
+  std::string className = CCompileInfo::GetPackage();
+  StringUtils::ToLower(className);
 
   if (dirname == "apps")
   {
