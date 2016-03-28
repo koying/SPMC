00f090e5403aa381d015d4ebf429ba6934c47b9e
Chris "Koying" Browet <cbro@semperpax.com>
2016-03-28 18:04:53 +0200

    FIX: alphanumeric sort: only do numeric sort for pure numeric

--- xbmc/utils/StringUtils.cpp
+++ xbmc/utils/StringUtils.cpp
@@ -847,17 +847,19 @@ int StringUtils::FindNumber(const std::string& strInput, const std::string &strF
 // and 0 if they are identical (essentially calculates left - right)
 int64_t StringUtils::AlphaNumericCompare(const wchar_t *left, const wchar_t *right)
 {
+  bool isNumeric = true;
   wchar_t *l = (wchar_t *)left;
   wchar_t *r = (wchar_t *)right;
-  wchar_t *ld, *rd;
+  wchar_t *ld = nullptr, *rd = nullptr;
   wchar_t lc, rc;
   int64_t lnum, rnum;
   const std::collate<wchar_t>& coll = std::use_facet<std::collate<wchar_t> >(g_langInfo.GetSystemLocale());
   int cmp_res = 0;
+
   while (*l != 0 && *r != 0)
   {
     // check if we have a numerical value
-    if (*l >= L'0' && *l <= L'9' && *r >= L'0' && *r <= L'9')
+    if (isNumeric && *l >= L'0' && *l <= L'9' && *r >= L'0' && *r <= L'9')
     {
       ld = l;
       lnum = 0;
@@ -874,9 +876,19 @@ int64_t StringUtils::AlphaNumericCompare(const wchar_t *left, const wchar_t *rig
         rnum += *rd++ - L'0';
       }
       // do we have numbers?
-      if (lnum != rnum)
-      { // yes - and they're different!
-        return lnum - rnum;
+      if ((*ld == 0 || *ld == L' ') && (*rd == 0 || *rd == L' '))
+      {
+        if (lnum != rnum)
+          return lnum - rnum;
+      }
+      else
+      {
+        // Restart
+        isNumeric = false;
+        ld = rd = nullptr;
+        l = (wchar_t *)left;
+        r = (wchar_t *)right;
+        continue;
       }
       l = ld;
       r = rd;
@@ -885,10 +897,10 @@ int64_t StringUtils::AlphaNumericCompare(const wchar_t *left, const wchar_t *rig
     // do case less comparison
     lc = *l;
     if (lc >= L'A' && lc <= L'Z')
-      lc += L'a'-L'A';
+      lc += L'a' - L'A';
     rc = *r;
     if (rc >= L'A' && rc <= L'Z')
-      rc += L'a'- L'A';
+      rc += L'a' - L'A';
 
     // ok, do a normal comparison, taking current locale into account. Add special case stuff (eg '(' characters)) in here later
     if ((cmp_res = coll.compare(&lc, &lc + 1, &rc, &rc + 1)) != 0)
