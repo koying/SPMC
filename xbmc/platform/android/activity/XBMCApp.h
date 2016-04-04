91a60dcb9c28f7624c13945d1e80585840ecfe7a
Chris "Koying" Browet <cbro@semperpax.com>
2016-04-04 21:00:58 +0200

    ADD: [voicerecog] use voice search button on soft keyboard
    Trigger voice recognition when Soft Keyboard is open by pressing the
    voice search button of the remotes

--- xbmc/platform/android/activity/XBMCApp.h
+++ xbmc/platform/android/activity/XBMCApp.h
@@ -35,6 +35,8 @@
 #include <androidjni/View.h>
 
 #include "threads/Event.h"
+#include "interfaces/IAnnouncer.h"
+
 #include "guilib/Geometry.h"
 #include "IActivityHandler.h"
 #include "IInputHandler.h"
@@ -82,14 +84,20 @@ protected:
   int m_resultcode;
 };
 
-class CXBMCApp : public IActivityHandler, public CJNIMainActivity,
-                 public CJNIBroadcastReceiver,
-                 public CJNIAudioManagerAudioFocusChangeListener
+class CXBMCApp
+    : public IActivityHandler
+    , public CJNIMainActivity
+    , public CJNIBroadcastReceiver
+    , public CJNIAudioManagerAudioFocusChangeListener
+    , public ANNOUNCEMENT::IAnnouncer
 {
 public:
   CXBMCApp(ANativeActivity *nativeActivity);
   virtual ~CXBMCApp();
   static CXBMCApp* get() { return m_xbmcappinstance; }
+
+  // IAnnouncer IF
+  virtual void Announce(ANNOUNCEMENT::AnnouncementFlag flag, const char *sender, const char *message, const CVariant &data);
 
   virtual void onReceive(CJNIIntent intent);
   virtual void onNewIntent(CJNIIntent intent);
