f1f08a815023ae1782b3071804a57304f5eaedf6
S. Davilla <davilla@4pi.com>
2016-06-03 11:01:06 -0400

    upstream sync, init ffmpeg threads like LAVFilters does #9899 and fix for android

--- xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecFFmpeg.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecFFmpeg.cpp
@@ -59,6 +59,9 @@
 #ifdef TARGET_DARWIN_OSX
 #include "VDA.h"
 #endif
+#if defined(TARGET_ANDROID)
+#include "platform/android/activity/AndroidFeatures.h"
+#endif
 #include "utils/StringUtils.h"
 
 extern "C" {
@@ -264,9 +267,13 @@ bool CDVDVideoCodecFFmpeg::Open(CDVDStreamInfo &hints, CDVDCodecOptions &options
     }
     else
     {
-      int num_threads = std::min(8 /*MAX_THREADS*/, g_cpuInfo.getCPUCount());
-      if( num_threads > 1)
-        m_pCodecContext->thread_count = num_threads;
+#if defined(TARGET_ANDROID)
+      int num_threads = CAndroidFeatures::GetCPUCount() * 3 / 2;
+#else
+      int num_threads = av_cpu_count() * 3 / 2;
+#endif
+      num_threads = std::max(1, std::min(num_threads, 16));
+      m_pCodecContext->thread_count = num_threads;
       m_pCodecContext->thread_safe_callbacks = 1;
       m_decoderState = STATE_SW_MULTI;
       CLog::Log(LOGDEBUG, "CDVDVideoCodecFFmpeg - open frame threaded with %d threads", num_threads);
