turulli/test

VideoPlayer: pass CFileItem to input stream

xbmc/cores/VideoPlayer/DVDInputStreams/DVDInputStreamBluray.cpp
@@ -183,8 +183,8 @@ void  bluray_overlay_argb_cb(void *this_gen, const struct bd_argb_overlay_s * co
 }
 #endif
 
-CDVDInputStreamBluray::CDVDInputStreamBluray(IVideoPlayer* player) :
-  CDVDInputStream(DVDSTREAM_TYPE_BLURAY)
+CDVDInputStreamBluray::CDVDInputStreamBluray(IVideoPlayer* player, CFileItem& fileitem) :
+  CDVDInputStream(DVDSTREAM_TYPE_BLURAY, fileitem)
 {
   m_title = NULL;
   m_clip  = (uint32_t)-1;
@@ -260,12 +260,12 @@ BLURAY_TITLE_INFO* CDVDInputStreamBluray::GetTitleFile(const std::string& filena
 }
 
 
-bool CDVDInputStreamBluray::Open(const char* strFile, const std::string& content, bool contentLookup)
+bool CDVDInputStreamBluray::Open()
 {
   if(m_player == NULL)
     return false;
 
-  std::string strPath(strFile);
+  std::string strPath(m_item.GetPath());
   std::string filename;
   std::string root;
 
@@ -298,8 +298,8 @@ bool CDVDInputStreamBluray::Open(const char* strFile, const std::string& content
       strPath = URIUtils::GetDirectory(strPath);
       URIUtils::RemoveSlashAtEnd(strPath);
     }
-    root     = strPath;
-    filename = URIUtils::GetFileName(strFile);
+    root = strPath;
+    filename = URIUtils::GetFileName(m_item.GetPath());
   }
 
   // root should not have trailing slash

xbmc/cores/VideoPlayer/DVDInputStreams/DVDInputStreamBluray.h
@@ -47,9 +47,9 @@ class CDVDInputStreamBluray
   , public CDVDInputStream::IMenus
 {
 public:
-  CDVDInputStreamBluray(IVideoPlayer* player);
+  CDVDInputStreamBluray(IVideoPlayer* player, CFileItem& fileitem);
   virtual ~CDVDInputStreamBluray();
-  virtual bool Open(const char* strFile, const std::string &content, bool contentLookup);
+  virtual bool Open();
   virtual void Close();
   virtual int Read(uint8_t* buf, int buf_size);
   virtual int64_t Seek(int64_t offset, int whence);

xbmc/cores/VideoPlayer/DVDInputStreams/DVDInputStreamNavigator.h
@@ -74,10 +74,10 @@ class CDVDInputStreamNavigator
   , public CDVDInputStream::IMenus
 {
 public:
-  CDVDInputStreamNavigator(IVideoPlayer* player);
+  CDVDInputStreamNavigator(IVideoPlayer* player, CFileItem& fileitem);
   virtual ~CDVDInputStreamNavigator();
 
-  virtual bool Open(const char* strFile, const std::string& content, bool contentLookup);
+  virtual bool Open();
   virtual void Close();
   virtual int Read(uint8_t* buf, int buf_size);
   virtual int64_t Seek(int64_t offset, int whence);

xbmc/cores/VideoPlayer/DVDInputStreams/DVDInputStreamPVRManager.cpp
@@ -38,7 +38,8 @@ using namespace PVR;
  * Description: Class constructor, initialize member variables
  *              public class is CDVDInputStream
  */
-CDVDInputStreamPVRManager::CDVDInputStreamPVRManager(IVideoPlayer* pPlayer) : CDVDInputStream(DVDSTREAM_TYPE_PVRMANAGER)
+CDVDInputStreamPVRManager::CDVDInputStreamPVRManager(IVideoPlayer* pPlayer, CFileItem& fileitem)
+  : CDVDInputStream(DVDSTREAM_TYPE_PVRMANAGER, fileitem)
 {
   m_pPlayer         = pPlayer;
   m_pFile           = NULL;
@@ -74,7 +75,7 @@ bool CDVDInputStreamPVRManager::IsEOF()
     return !m_pFile || m_eof;
 }
 
-bool CDVDInputStreamPVRManager::Open(const char* strFile, const std::string& content, bool contentLookup)
+bool CDVDInputStreamPVRManager::Open()
 {
   /* Open PVR File for both cases, to have access to ILiveTVInterface and
    * IRecordable
@@ -83,8 +84,10 @@ bool CDVDInputStreamPVRManager::Open(const char* strFile, const std::string& con
   m_pLiveTV     = ((CPVRFile*)m_pFile)->GetLiveTV();
   m_pRecordable = ((CPVRFile*)m_pFile)->GetRecordable();
 
-  CURL url(strFile);
-  if (!CDVDInputStream::Open(strFile, content, contentLookup)) return false;
+  if (!CDVDInputStream::Open())
+    return false;
+
+  CURL url(m_item.GetPath());
   if (!m_pFile->Open(url))
   {
     delete m_pFile;
@@ -105,19 +108,18 @@ bool CDVDInputStreamPVRManager::Open(const char* strFile, const std::string& con
    * for the right protocol stream handler and swap every call to this input stream
    * handler.
    */
-  std::string transFile = XFILE::CPVRFile::TranslatePVRFilename(strFile);
+  std::string transFile = XFILE::CPVRFile::TranslatePVRFilename(m_item.GetPath());
   if(transFile.substr(0, 6) != "pvr://")
   {
-    m_pOtherStream = CDVDFactoryInputStream::CreateInputStream(m_pPlayer, transFile, content);
+    m_item.SetPath(transFile);
+    m_pOtherStream = CDVDFactoryInputStream::CreateInputStream(m_pPlayer, m_item);
     if (!m_pOtherStream)
     {
       CLog::Log(LOGERROR, "CDVDInputStreamPVRManager::Open - unable to create input stream for [%s]", transFile.c_str());
       return false;
     }
-    else
-      m_pOtherStream->SetFileItem(m_item);
 
-    if (!m_pOtherStream->Open(transFile.c_str(), content, contentLookup))
+    if (!m_pOtherStream->Open())
     {
       CLog::Log(LOGERROR, "CDVDInputStreamPVRManager::Open - error opening [%s]", transFile.c_str());
       delete m_pFile;
@@ -131,8 +133,6 @@ bool CDVDInputStreamPVRManager::Open(const char* strFile, const std::string& con
   }
 
   ResetScanTimeout((unsigned int) CSettings::GetInstance().GetInt(CSettings::SETTING_PVRPLAYBACK_SCANTIME) * 1000);
-  m_content = content;
-  m_contentLookup = contentLookup;
   CLog::Log(LOGDEBUG, "CDVDInputStreamPVRManager::Open - stream opened: %s", transFile.c_str());
 
   return true;
@@ -374,7 +374,8 @@ bool CDVDInputStreamPVRManager::CloseAndOpen(const char* strFile)
 {
   Close();
 
-  if (Open(strFile, m_content, m_contentLookup))
+  m_item.SetPath(strFile);
+  if (Open())
   {
     return true;
   }

xbmc/cores/VideoPlayer/DVDInputStreams/DVDInputStreamPVRManager.h
@@ -43,9 +43,9 @@ class CDVDInputStreamPVRManager
   , public CDVDInputStream::ISeekable
 {
 public:
-  CDVDInputStreamPVRManager(IVideoPlayer* pPlayer);
+  CDVDInputStreamPVRManager(IVideoPlayer* pPlayer, CFileItem& fileitem);
   virtual ~CDVDInputStreamPVRManager();
-  virtual bool Open(const char* strFile, const std::string &content, bool contentLookup);
+  virtual bool Open();
   virtual void Close();
   virtual int Read(uint8_t* buf, int buf_size);
   virtual int64_t Seek(int64_t offset, int whence);

koying/SPMC

xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamBluray.cpp
@@ -183,8 +183,8 @@ void  bluray_overlay_argb_cb(void *this_gen, const struct bd_argb_overlay_s * co
 }
 #endif
 
-CDVDInputStreamBluray::CDVDInputStreamBluray(IDVDPlayer* player) :
-  CDVDInputStream(DVDSTREAM_TYPE_BLURAY)
+CDVDInputStreamBluray::CDVDInputStreamBluray(IDVDPlayer* player, CFileItem& fileitem) :
+  CDVDInputStream(DVDSTREAM_TYPE_BLURAY, fileitem)
 {
   m_title = NULL;
   m_clip  = (uint32_t)-1;
@@ -260,12 +260,12 @@ BLURAY_TITLE_INFO* CDVDInputStreamBluray::GetTitleFile(const std::string& filena
 }
 
 
-bool CDVDInputStreamBluray::Open(const char* strFile, const std::string& content, bool contentLookup)
+bool CDVDInputStreamBluray::Open()
 {
   if(m_player == NULL)
     return false;
 
-  std::string strPath(strFile);
+  std::string strPath(m_item.GetPath());
   std::string filename;
   std::string root;
 
@@ -298,8 +298,8 @@ bool CDVDInputStreamBluray::Open(const char* strFile, const std::string& content
       strPath = URIUtils::GetDirectory(strPath);
       URIUtils::RemoveSlashAtEnd(strPath);
     }
-    root     = strPath;
-    filename = URIUtils::GetFileName(strFile);
+    root = strPath;
+    filename = URIUtils::GetFileName(m_item.GetPath());
   }
 
   // root should not have trailing slash

xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamBluray.h
@@ -47,9 +47,9 @@ class CDVDInputStreamBluray
   , public CDVDInputStream::IMenus
 {
 public:
-  CDVDInputStreamBluray(IDVDPlayer* player);
+  CDVDInputStreamBluray(IDVDPlayer* player, CFileItem& fileitem);
   virtual ~CDVDInputStreamBluray();
-  virtual bool Open(const char* strFile, const std::string &content, bool contentLookup);
+  virtual bool Open();
   virtual void Close();
   virtual int Read(uint8_t* buf, int buf_size);
   virtual int64_t Seek(int64_t offset, int whence);

xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamNavigator.h
@@ -74,10 +74,10 @@ class CDVDInputStreamNavigator
   , public CDVDInputStream::IMenus
 {
 public:
-  CDVDInputStreamNavigator(IDVDPlayer* player);
+  CDVDInputStreamNavigator(IDVDPlayer* player, CFileItem& fileitem);
   virtual ~CDVDInputStreamNavigator();
 
-  virtual bool Open(const char* strFile, const std::string& content, bool contentLookup);
+  virtual bool Open();
   virtual void Close();
   virtual int Read(uint8_t* buf, int buf_size);
   virtual int64_t Seek(int64_t offset, int whence);

xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamPVRManager.cpp
@@ -38,7 +38,8 @@ using namespace PVR;
  * Description: Class constructor, initialize member variables
  *              public class is CDVDInputStream
  */
-CDVDInputStreamPVRManager::CDVDInputStreamPVRManager(IDVDPlayer* pPlayer) : CDVDInputStream(DVDSTREAM_TYPE_PVRMANAGER)
+CDVDInputStreamPVRManager::CDVDInputStreamPVRManager(IDVDPlayer* pPlayer, CFileItem& fileitem)
+  : CDVDInputStream(DVDSTREAM_TYPE_PVRMANAGER, fileitem)
 {
   m_pPlayer         = pPlayer;
   m_pFile           = NULL;
@@ -74,7 +75,7 @@ bool CDVDInputStreamPVRManager::IsEOF()
     return !m_pFile || m_eof;
 }
 
-bool CDVDInputStreamPVRManager::Open(const char* strFile, const std::string& content, bool contentLookup)
+bool CDVDInputStreamPVRManager::Open()
 {
   /* Open PVR File for both cases, to have access to ILiveTVInterface and
    * IRecordable
@@ -83,8 +84,10 @@ bool CDVDInputStreamPVRManager::Open(const char* strFile, const std::string& con
   m_pLiveTV     = ((CPVRFile*)m_pFile)->GetLiveTV();
   m_pRecordable = ((CPVRFile*)m_pFile)->GetRecordable();
 
-  CURL url(strFile);
-  if (!CDVDInputStream::Open(strFile, content, contentLookup)) return false;
+  if (!CDVDInputStream::Open())
+    return false;
+
+  CURL url(m_item.GetPath());
   if (!m_pFile->Open(url))
   {
     delete m_pFile;
@@ -105,19 +108,18 @@ bool CDVDInputStreamPVRManager::Open(const char* strFile, const std::string& con
    * for the right protocol stream handler and swap every call to this input stream
    * handler.
    */
-  std::string transFile = XFILE::CPVRFile::TranslatePVRFilename(strFile);
+  std::string transFile = XFILE::CPVRFile::TranslatePVRFilename(m_item.GetPath());
   if(transFile.substr(0, 6) != "pvr://")
   {
-    m_pOtherStream = CDVDFactoryInputStream::CreateInputStream(m_pPlayer, transFile, content);
+    m_item.SetPath(transFile);
+    m_pOtherStream = CDVDFactoryInputStream::CreateInputStream(m_pPlayer, m_item);
     if (!m_pOtherStream)
     {
       CLog::Log(LOGERROR, "CDVDInputStreamPVRManager::Open - unable to create input stream for [%s]", CURL::GetRedacted(transFile).c_str());
       return false;
     }
-    else
-      m_pOtherStream->SetFileItem(m_item);
 
-    if (!m_pOtherStream->Open(transFile.c_str(), content, contentLookup))
+    if (!m_pOtherStream->Open())
     {
       CLog::Log(LOGERROR, "CDVDInputStreamPVRManager::Open - error opening [%s]", CURL::GetRedacted(transFile).c_str());
       delete m_pFile;
@@ -131,8 +133,6 @@ bool CDVDInputStreamPVRManager::Open(const char* strFile, const std::string& con
   }
 
   ResetScanTimeout((unsigned int) CSettings::GetInstance().GetInt(CSettings::SETTING_PVRPLAYBACK_SCANTIME) * 1000);
-  m_content = content;
-  m_contentLookup = contentLookup;
   CLog::Log(LOGDEBUG, "CDVDInputStreamPVRManager::Open - stream opened: %s", CURL::GetRedacted(transFile).c_str());
 
   return true;
@@ -379,7 +379,8 @@ bool CDVDInputStreamPVRManager::CloseAndOpen(const char* strFile)
 {
   Close();
 
-  if (Open(strFile, m_content, m_contentLookup))
+  m_item.SetPath(strFile);
+  if (Open())
   {
     return true;
   }

xbmc/cores/dvdplayer/DVDInputStreams/DVDInputStreamPVRManager.h
@@ -43,9 +43,9 @@ class CDVDInputStreamPVRManager
   , public CDVDInputStream::ISeekable
 {
 public:
-  CDVDInputStreamPVRManager(IDVDPlayer* pPlayer);
+  CDVDInputStreamPVRManager(IDVDPlayer* pPlayer, CFileItem& fileitem);
   virtual ~CDVDInputStreamPVRManager();
-  virtual bool Open(const char* strFile, const std::string &content, bool contentLookup);
+  virtual bool Open();
   virtual void Close();
   virtual int Read(uint8_t* buf, int buf_size);
   virtual int64_t Seek(int64_t offset, int whence);