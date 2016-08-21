8286afac9326b61ddc0602f8d5ce6f3a39c42546
Chris "Koying" Browet <cbro@semperpax.com>
2016-08-21 16:17:36 +0200

    wip

--- xbmc/android/activity/JNIMainActivity.h
+++ xbmc/android/activity/JNIMainActivity.h
@@ -45,6 +45,7 @@ public:
   static void registerMediaButtonEventReceiver();
   static void unregisterMediaButtonEventReceiver();
   static void screenOn();
+  static void startCrashHandler();
 
   CJNISurface getVideoViewSurface();
   void clearVideoView();
