7a91af0b685cabc340f84d940c7ec5ab51b42b38
Chris Koying Browet <cbro@semperpax.com>
2016-10-10 16:30:53 +0200

    Revert "ADD: [droid] capture stderr/stdout to logcat"
    This reverts commit 6678928f9329a0ae66d3e5a5c7dfd0af7bf61f76.

--- xbmc/platform/android/activity/android_main.cpp
+++ xbmc/platform/android/activity/android_main.cpp
@@ -97,48 +97,6 @@ static bool dumpCallback(const google_breakpad::MinidumpDescriptor& descriptor,
 }
 #endif
 
-
-// redirect stdout / stderr to logcat
-// https://codelab.wordpress.com/2014/11/03/how-to-use-standard-output-streams-for-logging-in-android-apps/
-static int pfd[2];
-static pthread_t thr;
-static const char *tag = "myapp";
-
-static void *thread_logger(void*)
-{
-  ssize_t rdsz;
-  char buf[128];
-  while((rdsz = read(pfd[0], buf, sizeof buf - 1)) > 0)
-  {
-    if(buf[rdsz - 1] == '\n')
-      --rdsz;
-    buf[rdsz] = 0;  /* add null-terminator */
-    __android_log_write(ANDROID_LOG_DEBUG, tag, buf);
-  }
-  return 0;
-}
-
-int start_logger(const char *app_name)
-{
-  tag = app_name;
-
-  /* make stdout line-buffered and stderr unbuffered */
-  setvbuf(stdout, 0, _IOLBF, 0);
-  setvbuf(stderr, 0, _IONBF, 0);
-
-  /* create the pipe and redirect stdout and stderr */
-  pipe(pfd);
-  dup2(pfd[1], 1);
-  dup2(pfd[1], 2);
-
-  /* spawn the logging thread */
-  if(pthread_create(&thr, 0, thread_logger, 0) == -1)
-    return -1;
-  pthread_detach(thr);
-  return 0;
-}
-
-
 // copied from new android_native_app_glue.c
 static void process_input(struct android_app* app, struct android_poll_source* source) {
     AInputEvent* event = NULL;
@@ -183,7 +141,6 @@ extern void android_main(struct android_app* state)
                                         true,
                                         -1);
 #endif
-      start_logger("SPMC_STD");
 
       IInputHandler inputHandler;
       eventLoop.run(xbmcApp, inputHandler);
