tonyonetone/xbmc

ADD: [filemanager] Show info and size for video db items

xbmc/windows/GUIWindowFileManager.h
@@ -65,6 +65,7 @@ protected:
   void OnRename(int iList);
   void OnSelectAll(int iList);
   void OnNewFolder(int iList);
+  void OnInfo(int iList);
   void Refresh();
   void Refresh(int iList);
   int GetSelectedItem(int iList);

koying/SPMC

xbmc/windows/GUIWindowFileManager.h
@@ -67,6 +67,7 @@ protected:
   void OnRename(int iList);
   void OnSelectAll(int iList);
   void OnNewFolder(int iList);
+  void OnShowInfo(int iList);
   void Refresh();
   void Refresh(int iList);
   int GetSelectedItem(int iList);