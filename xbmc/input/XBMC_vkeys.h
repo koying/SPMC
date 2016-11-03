2f5dee1580cf6fca272a47cbf68b50db99fec7c5
Chris "Koying" Browet <cbro@semperpax.com>
2016-11-03 19:10:57 +0100

    ADD: [droid;key] channel up/down

--- xbmc/input/XBMC_vkeys.h
+++ xbmc/input/XBMC_vkeys.h
@@ -222,6 +222,8 @@ typedef enum {
   XBMCVK_HOMEPAGE       = 0xEA,
   XBMCVK_CONFIG         = 0xEB,
   XBMCVK_EPG            = 0xEC,
+  XBMCVK_CHANUP         = 0xED,
+  XBMCVK_CHANDOWN       = 0xEE,
 
   XBMCVK_LAST           = 0xFF
 } XBMCVKey;
