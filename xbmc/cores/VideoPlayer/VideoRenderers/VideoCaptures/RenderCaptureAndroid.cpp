1cdd2cb5e6798dd5be786c733afd9993af25afa7
Chris "Koying" Browet <cbro@semperpax.com>
2017-07-01 19:38:52 +0200

    FIX: [droid;capture] trap empty images

--- xbmc/cores/VideoPlayer/VideoRenderers/VideoCaptures/RenderCaptureAndroid.cpp
+++ xbmc/cores/VideoPlayer/VideoRenderers/VideoCaptures/RenderCaptureAndroid.cpp
@@ -90,6 +90,11 @@ void CRenderCaptureAndroid::ReadOut()
       int iHeight = image.getHeight();
 
       std::vector<jni::CJNIImagePlane> planes = image.getPlanes();
+      if (planes.empty())
+      {
+        SetState(CAPTURESTATE_FAILED);
+        return;
+      }
       CJNIByteBuffer bytebuffer = planes[0].getBuffer();
 
       struct SwsContext *context = sws_getContext(iWidth, iHeight, AV_PIX_FMT_RGBA,
