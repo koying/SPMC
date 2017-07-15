f2ee5a79f6eccccf1f622582a6d3c2683cc4aecc
Chris "Koying" Browet <cbro@semperpax.com>
2017-07-15 09:58:58 +0200

    Revert "ADD: [SMB] allow to force V1 protocol"
    This reverts commit e3c9d4e003f622dd3c938dd780a9b03a10ff744d.

--- xbmc/settings/Settings.cpp
+++ xbmc/settings/Settings.cpp
@@ -340,7 +340,6 @@ const std::string CSettings::SETTING_SERVICES_AIRPLAYPASSWORD = "services.airpla
 const std::string CSettings::SETTING_SERVICES_AIRPLAYVIDEOSUPPORT = "services.airplayvideosupport";
 const std::string CSettings::SETTING_SMB_WINSSERVER = "smb.winsserver";
 const std::string CSettings::SETTING_SMB_WORKGROUP = "smb.workgroup";
-const std::string CSettings::SETTING_SMB_FORCEV1 = "smb.forcev1";
 const std::string CSettings::SETTING_SMB_OVERWRITECONF = "smb.overwriteconf";
 const std::string CSettings::SETTING_VIDEOSCREEN_MONITOR = "videoscreen.monitor";
 const std::string CSettings::SETTING_VIDEOSCREEN_SCREEN = "videoscreen.screen";
@@ -1241,7 +1240,6 @@ void CSettings::InitializeISettingCallbacks()
   settingSet.insert(CSettings::SETTING_SERVICES_ESCONTINUOUSDELAY);
   settingSet.insert(CSettings::SETTING_SMB_WINSSERVER);
   settingSet.insert(CSettings::SETTING_SMB_WORKGROUP);
-  settingSet.insert(CSettings::SETTING_SMB_FORCEV1);
   settingSet.insert(CSettings::SETTING_SMB_OVERWRITECONF);
   m_settingsManager->RegisterCallback(&CNetworkServices::GetInstance(), settingSet);
 
