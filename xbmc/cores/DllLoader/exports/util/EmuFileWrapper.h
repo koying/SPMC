0fd1e333b9510cc635e9a5b9a2f93b88fa0426cb
Chris Koying Browet <cbro@semperpax.com>
2017-07-18 11:55:51 +0200

    CHG: simplify emu_msvcrt FILE* handling
    Those FILE* are all ours and opaque.
    Let's take advantage of it, rather than use FILE opaque fields

--- xbmc/cores/DllLoader/exports/util/EmuFileWrapper.h
+++ xbmc/cores/DllLoader/exports/util/EmuFileWrapper.h
@@ -32,7 +32,7 @@
 #endif
 
 #define MAX_EMULATED_FILES    50
-#define FILE_WRAPPER_OFFSET   0x0200
+#define FILE_WRAPPER_OFFSET   0x00000200
 
 namespace XFILE
 {
@@ -41,13 +41,12 @@ namespace XFILE
 
 typedef struct stEmuFileObject
 {
-  FILE    file_emu;
-  int     fd;
   XFILE::CFile*  file_xbmc;
   CCriticalSection *file_lock;
   int mode;
   //Stick this last to avoid 3-7 bytes of padding
   bool    used;
+  int     fd;
 } EmuFileObject;
 
 class CEmuFileWrapper
@@ -63,10 +62,12 @@ public:
 
   EmuFileObject* RegisterFileObject(XFILE::CFile* pFile);
   void UnRegisterFileObjectByDescriptor(int fd);
+  void UnRegisterFileObjectByStream(FILE* stream);
   void LockFileObjectByDescriptor(int fd);
   bool TryLockFileObjectByDescriptor(int fd);
   void UnlockFileObjectByDescriptor(int fd);
   EmuFileObject* GetFileObjectByDescriptor(int fd);
+  EmuFileObject* GetFileObjectByStream(FILE* stream);
   XFILE::CFile* GetFileXbmcByDescriptor(int fd);
   XFILE::CFile* GetFileXbmcByStream(FILE* stream);
   static int GetDescriptorByStream(FILE* stream);
@@ -77,7 +78,7 @@ public:
   }
   static bool StreamIsEmulatedFile(FILE* stream);
 private:
-  static EmuFileObject m_files[MAX_EMULATED_FILES];
+  EmuFileObject m_files[MAX_EMULATED_FILES];
   CCriticalSection m_criticalSection;
 };
 
