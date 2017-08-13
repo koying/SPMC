b1b48c6c399af03576bd306cf35167a5b90a964f
Chris "Koying" Browet <cbro@semperpax.com>
2017-08-13 16:26:35 +0200

    fixup amcs debug

--- xbmc/cores/VideoPlayer/VideoRenderers/HwDecRender/RendererMediaCodecSurface.cpp
+++ xbmc/cores/VideoPlayer/VideoRenderers/HwDecRender/RendererMediaCodecSurface.cpp
@@ -59,9 +59,10 @@ void CRendererMediaCodecSurface::AddVideoPictureHW(DVDVideoPicture &picture, int
     buf.hwDec = picture.mediacodec->Retain();
     if (m_readyToRender)
     {
-//    CLog::Log(LOGDEBUG, "addpic: cc(%f), ch(%f), diff(%f), pts(%f), ptsdiff(%f)", currentClock/1000.0, CurrentHostCounter()/1000000.0, (currentClock/1000.0) - (CurrentHostCounter()/1000000.0), picture.pts / 1000.0, (picture.pts - currentClock) / 1000.0);
-    int64_t nanodiff(static_cast<int64_t>((picture.pts - picture.clock->GetClock()) * 1000));
-    picture.mediacodec->ReleaseOutputBuffer(true, CurrentHostCounter() + nanodiff);
+      double currentClock = picture.clock->GetClock();
+//      CLog::Log(LOGDEBUG, "addpic: cc(%f), ch(%f), diff(%f), pts(%f), ptsdiff(%f)", currentClock/1000.0, CurrentHostCounter()/1000000.0, (currentClock/1000.0) - (CurrentHostCounter()/1000000.0), picture.pts / 1000.0, (picture.pts - currentClock) / 1000.0);
+      int64_t nanodiff(static_cast<int64_t>((picture.pts - currentClock) * 1000));
+      picture.mediacodec->ReleaseOutputBuffer(true, CurrentHostCounter() + nanodiff);
     }
     else
       picture.mediacodec->ReleaseOutputBuffer(false);
