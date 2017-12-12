ef1100e672e4d82318dc3fff0a0069e998719546
Chris "Koying" Browet <cbro@semperpax.com>
2017-12-12 08:52:32 +0100

    wip

--- project/cmake/scripts/android/Install.cmake
+++ project/cmake/scripts/android/Install.cmake
@@ -52,14 +52,14 @@ set(package_files strings.xml
                   src/interfaces/XBMCNsdManagerDiscoveryListener.java
                   src/interfaces/XBMCNsdManagerRegistrationListener.java
                   src/interfaces/XBMCNsdManagerResolveListener.java
-                  channels/model/Media.java
-                  channels/model/Subscription.java
-                  channels/model/XBMCDatabase.java
-                  channels/SyncChannelJobService.java
-                  channels/SyncProgramsJobService.java
-                  channels/util/AppLinkHelper.java
-                  channels/util/SharedPreferencesHelper.java
-                  channels/util/TvUtil.java
+                  src/channels/model/Media.java
+                  src/channels/model/Subscription.java
+                  src/channels/model/XBMCDatabase.java
+                  src/channels/SyncChannelJobService.java
+                  src/channels/SyncProgramsJobService.java
+                  src/channels/util/AppLinkHelper.java
+                  src/channels/util/SharedPreferencesHelper.java
+                  src/channels/util/TvUtil.java
                   )
 foreach(file IN LISTS package_files)
   configure_file(${CORE_SOURCE_DIR}/tools/android/packaging/xbmc/${file}.in
