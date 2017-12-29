e9eeb8be542cb95613806a38b898a5d39d2113fc
Chris "Koying" Browet <cbro@semperpax.com>
2017-12-29 13:13:40 +0100

    DEBUG: Appparamparser

--- xbmc/AppParamParser.cpp
+++ xbmc/AppParamParser.cpp
@@ -26,6 +26,9 @@
 #include "utils/SystemInfo.h"
 #include "utils/StringUtils.h"
 #include "input/InputManager.h"
+#include "messaging/ApplicationMessenger.h"
+#include "URL.h"
+
 #ifdef TARGET_WINDOWS
 #include "WIN32Util.h"
 #endif
@@ -142,10 +145,27 @@ void CAppParamParser::ParseArg(const std::string &arg)
     if (m_testmode)
       g_application.SetEnableTestMode(true);
 
-    CFileItemPtr pItem(new CFileItem(arg));
-    pItem->SetPath(arg);
+    std::string targetFile = arg;
+    CURL targeturl(targetFile);
 
-    m_playlist.Add(pItem);
+    if (targeturl.IsProtocol("videodb")
+      || (targeturl.IsProtocol("special") && targetFile.find("playlists/video") != std::string::npos)
+      || (targeturl.IsProtocol("special") && targetFile.find("playlists/mixed") != std::string::npos)
+      )
+    {
+      std::vector<std::string> params;
+      params.push_back(targeturl.Get());
+      params.push_back("return");
+      CApplicationMessenger::GetInstance().PostMsg(TMSG_GUI_ACTIVATE_WINDOW, WINDOW_VIDEO_NAV, 0, nullptr, "", params);
+    }
+    else if (targeturl.IsProtocol("musicdb")
+      || (targeturl.IsProtocol("special") && targetFile.find("playlists/music") != std::string::npos))
+    {
+      std::vector<std::string> params;
+      params.push_back(targeturl.Get());
+      params.push_back("return");
+      CApplicationMessenger::GetInstance().PostMsg(TMSG_GUI_ACTIVATE_WINDOW, WINDOW_MUSIC_NAV, 0, nullptr, "", params);
+    }
   }
 }
 
